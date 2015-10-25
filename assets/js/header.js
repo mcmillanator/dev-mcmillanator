ba42b99b2aff69f031c6aa1ddf60f9f1f355ce3b
Stanley McMillan <stanalanmcm@gmail.com>
2015-10-24 23:32:03 -0400

    completed the about section's style

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,23 +3,32 @@
     var header,
         headerRec,
         about,
-        aboutRec;
+        aboutRec,
+        aboutInfo,
+        aboutInfoRec;
     // prevent js from loading too early
     document.addEventListener("load", mainFunction());
     // assign vars with correct element dimentions and create scroll event
     function mainFunction() {
       header        = document.getElementById("header");
-      headerRec = header.getBoundingClientRect();
+      headerRec     = header.getBoundingClientRect();
       about         = document.getElementsByClassName("about");
+      aboutInfo     = document.getElementsByClassName("about-info")[0];
       window.addEventListener("scroll", changeClass);
     }
     function changeClass() {
-      aboutRec  = about[0].getBoundingClientRect();
+      aboutRec        = about[0].getBoundingClientRect();
+      aboutInfoRec   = aboutInfo.getBoundingClientRect();
       scroll = document.body.scrollTop;
-      if (0 + headerRec.bottom >= aboutRec.bottom) {
+      if (headerRec.bottom >= aboutRec.bottom) {
         header.className = "header header-background";
       } else if (headerRec.bottom < aboutRec.bottom) {
         header.className = "header";
+      }
+      if (aboutInfoRec.top+20 >= headerRec.bottom) {
+        aboutInfo.className = "about-info ";
+      } else {
+        aboutInfo.className = "about-info blur";
       }
     }
   }
