2500ca9f9eeff4a873784f51e1870d64878909ff
Stanley McMillan <stanalanmcm@gmail.com>
2015-10-30 17:37:45 -0400

    updated the js so that the header effect is in place and all other future effects are seperate

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,44 +5,15 @@
         about,
         aboutRec,
         aboutInfo,
-        aboutInfoRec,
-        skills,
-        skillsRec,
-        skillsPH,
-        skillsPHRec;
+        aboutInfoRec;
     // prevent js from loading too early
     document.addEventListener("load", mainFunction());
     // assign vars with correct element dimentions and create scroll event
     function mainFunction() {
       header          = document.getElementById("header");
-      skills          = document.getElementById("skills");
-      skillsPH        = document.getElementById("skills-placeholder");
-      skillsList      = document.getElementById("skills-list");
       about           = document.getElementById("about");
       aboutInfo       = document.getElementById("about-info");
-      projects        = document.getElementById("projects");
-      projectsSection = document.getElementById("projects-section");
-      projectsPH      = document.getElementById("projects-placeholder");
       window.addEventListener("scroll", changeClass);
-      window.addEventListener("scroll", getElements);
-    }
-
-    function getElements(){
-      dynamicClasses(header, skills, skillsList, skillsPH, "skills-heading", "skills-heading-fixed");
-      dynamicClasses(header, projects, projectsSection, projectsPH, "skills-heading", "skills-heading-fixed");
-    }
-
-    function dynamicClasses(el1, el2, el3, placeholder, class1, class2) {
-      el1         = el1.getBoundingClientRect();
-      el2Rec      = el2.getBoundingClientRect();
-      el3         = el3.getBoundingClientRect();
-      if ( el3.top >= el1.bottom + el2Rec.height) {
-        el2.className = class1;
-        placeholder.style.height = 0;
-      } else if (el2Rec.top <= el1.bottom + el2Rec.height) {
-        el2.className = class1 + " " + class2;
-        placeholder.style.height = el2Rec.height + "px";
-      }
     }
     function changeClass() {
       headerRec      = header.getBoundingClientRect();
